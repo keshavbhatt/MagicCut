f02dbe4778c4b229d687522d6da03813ff5a3e76
Keshav Bhatt <keshavnrj@gmail.com>
2023-03-17 13:35:00 +0530

    chore: add formatSize util method

--- src/app/utils.cpp
+++ src/app/utils.cpp
@@ -3,3 +3,21 @@
 Utils::Utils(QObject *parent)
     : QObject{parent}
 {}
+
+QString Utils::formatSize(qint64 sizeInBytes)
+{
+    static const QStringList suffixes = { "B", "KB", "MB", "GB", "TB" };
+
+    int suffixIndex = 0;
+    double size = static_cast<double>(sizeInBytes);
+
+    while (size >= 1024.0 && suffixIndex < suffixes.count() - 1) {
+        size /= 1024.0;
+        suffixIndex++;
+    }
+
+    QString result = QString::number(size, 'f', 2);
+    result.append(" ").append(suffixes.at(suffixIndex));
+
+    return result;
+}
--- src/app/utils.h
+++ src/app/utils.h
@@ -8,6 +8,18 @@ class Utils : public QObject
     Q_OBJECT
 public:
     explicit Utils(QObject *parent = nullptr);
+
+    /**
+     * @brief Converts a size in bytes to a human-readable string with SI unit suffixes.
+     *
+     * This method takes a qint64 value representing the size in bytes and converts it
+     * to a human-readable string with SI suffixes (e.g. "1.23 MB"). The size is rounded
+     * to two decimal places and the largest appropriate suffix is appended.
+     *
+     * @param sizeInBytes The size in bytes to be converted.
+     * @return A QString representing the size in a human-readable format.
+     */
+    static QString formatSize(qint64 sizeInBytes);
 };
 
 #endif // UTILS_H
